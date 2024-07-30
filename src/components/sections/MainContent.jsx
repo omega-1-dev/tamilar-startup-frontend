efee0b768bcf343486fe9e2a6d6b5bea6d6cbb3e
omega-1-dev <omega14202@gmail.com>
2024-07-30 21:18:39 +0530

    modified:MainContent.jsx-added out reach partner

--- src/components/sections/MainContent.jsx
+++ src/components/sections/MainContent.jsx
@@ -317,6 +317,13 @@ export default function MainContent() {
                 className="object-contain w-full h-full"
               />
             </div>
+            <div className="w-48 h-48 ">
+              <img
+                src="https://omega2024.sirv.com/other_assets/TheDotStudios_LOGO_PNG.png"
+                alt="sponsor1"
+                className="object-contain w-full h-full"
+              />
+            </div> 
           </div>
         </div>
 
